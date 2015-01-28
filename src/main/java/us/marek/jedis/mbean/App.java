9a8681e569a27f59473cf64fcc4a9fbb80f88b7d
Marek Kolodziej <marek.kolodziej@gonitro.con>
2015-01-28 09:58:54 -0800

    First commit

---
+++ src/main/java/us/marek/jedis/mbean/App.java
@@ -0,0 +1,79 @@
+package us.marek.jedis.mbean;
+
+import java.io.IOException;
+import java.lang.management.ManagementFactory;
+
+import javax.management.InstanceAlreadyExistsException;
+import javax.management.MBeanRegistrationException;
+import javax.management.MBeanServer;
+import javax.management.MalformedObjectNameException;
+import javax.management.NotCompliantMBeanException;
+import javax.management.ObjectName;
+
+import redis.clients.jedis.Client;
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+	
+	private static JedisPool pool = null;
+	private static Jedis jedis = null;
+	
+    public static void main(final String[] args) 
+    		throws InstanceAlreadyExistsException, MBeanRegistrationException,
+    		NotCompliantMBeanException, MalformedObjectNameException {
+    	
+    	Client client = null;
+    	
+    	try {
+    		
+			pool = new JedisPool(new JedisPoolConfig(), "localhost");
+			jedis = pool.getResource();
+			client = jedis.getClient();
+			client.connect();
+			
+			final MBeanServer mbs = ManagementFactory.getPlatformMBeanServer(); 
+	        final ObjectName name = new ObjectName("us.marek.jedis.mbean:type=JedisMBean"); 
+	        final JedisMBean mBean = new JedisMBean(jedis);
+	        mbs.registerMBean(mBean, name); 
+			
+			while (true) {
+				
+				try {
+				
+					
+				Thread.sleep(100);
+				
+				if (System.in.read() > 0) {
+					
+					break;
+				}
+
+				
+				} catch (final InterruptedException ie) {
+					
+					ie.printStackTrace();
+					
+				} catch (final IOException ioe) {
+					
+					ioe.printStackTrace();
+				}
+			}
+    		 
+    	} finally {
+    		
+    		jedis.close();
+    		pool.close();
+    		client.close();
+    		
+    	}
+        
+    }
+    
+    
+}
